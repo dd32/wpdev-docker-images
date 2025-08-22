3206422ea6df1842109db12d30aa7577b16dc436
John Blackbourn <john@johnblackbourn.com>
2025-08-22 22:15:39 +0100

    Rebuild.

--- images/8.5/php/common.sh
+++ images/8.5/php/common.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 set -e
 
-# PHP 8.5 beta1 - opcache, xdebug, and memcached are not yet compatible
-# Skip extension enabling for now
+# Check if an extension is available
+extension_available() {
+	local ext=$1
+	if [ -f "/usr/local/lib/php/extensions/no-debug-non-zts-$(php -r 'echo PHP_EXTENSION_DIR;' | xargs basename)/${ext}.so" ]; then
+		return 0
+	fi
+	return 1
+}
+
+# If LOCAL_PHP_XDEBUG=true xdebug extension will be enabled
+if [ "$LOCAL_PHP_XDEBUG" = true ]; then
+	if extension_available "xdebug"; then
+		docker-php-ext-enable xdebug
+		rm -f /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
+	else
+		echo "Warning: xdebug extension not available, skipping..."
+	fi
+else
+	if extension_available "opcache"; then
+		docker-php-ext-enable opcache
+		rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+	else
+		# OPcache might be built-in, check if it's already loaded
+		if ! php -m | grep -q "Zend OPcache"; then
+			echo "Warning: opcache extension not available, skipping..."
+		fi
+	fi
+fi
+
+# If LOCAL_PHP_MEMCACHED=true memcached extension will be enabled
+if [ "$LOCAL_PHP_MEMCACHED" = true ]; then
+	if extension_available "memcached"; then
+		docker-php-ext-enable memcached
+	else
+		echo "Warning: memcached extension not available, skipping..."
+	fi
+else
+	rm -f /usr/local/etc/php/conf.d/docker-php-ext-memcached.ini
+fi
 
 ### Change UID/GID
 WP_PHP_UID="${PHP_FPM_UID-1000}"
